63ef842071d9ff194cf1b136418bb243879e1129
dev3team <posokhovrom@gmail.com>
2023-09-22 21:09:04 +0300

    changed name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
+const name = "I'm Next.JS Developer";
 export const siteTitle = 'Next.js Sample Website';
 
 
